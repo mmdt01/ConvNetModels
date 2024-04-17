717dd6a644fc8b650845ad991806f0a3f936803f
Matthys du Toit <mmdt20@bath.ac.uk>
2024-04-17 12:09:22 +0100

    eegnet trained and evaluated k-fold

--- main2.py
+++ main2.py
@@ -8,7 +8,7 @@ import mne
 from mne import io
 from sklearn.model_selection import KFold
 import numpy as np
-
+from matplotlib import pyplot as plt
 # EEGNet-specific imports
 from tensorflow import keras
 from EEGModels import EEGNet
@@ -16,18 +16,11 @@ from keras import utils as np_utils
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
 
-# tools for plotting confusion matrices
-from matplotlib import pyplot as plt
-
 ##################### Load and epoch data #####################
 
 # define data path
 data_path = "subject_4"
 file_name = "s4_preprocessed.fif"
-
-# load the preprocessed data
-raw = mne.io.read_raw_fif(data_path + "/" + file_name, preload=True)
-fs = raw.info.get('sfreq')
 
 # create an event dictionary
 event_dict = {
@@ -41,155 +34,113 @@ event_dict = {
     'imagery and perception down': 8
 }
 
-# extract the event information from the raw data
-events, event_ids = mne.events_from_annotations(raw, event_id=event_dict)
-# extract epochs from the raw data
-epochs = mne.Epochs(raw, events, event_id=[5,6], tmin=0, tmax=3, baseline=None, preload=True)
-# shuffle the epochs
-permutation = np.random.permutation(len(epochs))
-epochs = epochs[permutation]
+# function for calculating the recall metric
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
 
-# # plot the epochs
-# epochs.plot(n_channels=16, scalings={"eeg": 20}, title="Epochs", n_epochs=5, events=True)
-# print(epochs)
-# print(len(epochs))
-# plt.show()
-# print(epochs.drop_log) 
+# function for calculating the precision metric
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
 
-# extract and normalize the labels ensuring they start from 1
-labels = epochs.events[:, -1] - min(epochs.events[:, -1]) + 1
-print(labels)
+# function for calculating the F1-score metric
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-# extract raw data. scale by 1000 due to scaling sensitivity in deep learning
-X = epochs.get_data()*1000 # format is in (trials, channels, samples)
-y = labels
+# function for loading the raw data and preparing it into labels and epochs
+def prepare_data(data_path, file_name, event_dict, class_labels, tmin, tmax):
+    # load the preprocessed data
+    raw = mne.io.read_raw_fif(data_path + "/" + file_name, preload=True)
+    # extract the event information from the raw data
+    events, event_ids = mne.events_from_annotations(raw, event_id=event_dict)
+    # extract epochs from the raw data
+    epochs = mne.Epochs(raw, events, event_id=class_labels, tmin=tmin, tmax=tmax, baseline=None, preload=True)
+    # plot the epochs (optional)
+    epochs.plot(n_channels=16, scalings={"eeg": 20}, title="Epochs", n_epochs=5, events=True)
+    print(epochs)
+    plt.show()
+    # extract and normalize the labels ensuring they start from 1
+    labels = epochs.events[:, -1] - min(epochs.events[:, -1]) + 1
+    # extract raw data. scale by 1000 due to scaling sensitivity in deep learning
+    X = epochs.get_data()*1e6 # format is in (trials, channels, samples)
+    y = labels
+    # print the shape of the epochs and labels
+    print('------------------------------------------------------------------------')
+    print("Shape of data: ", X.shape)
+    print('------------------------------------------------------------------------')
+    print("Shape of labels: ", y.shape)
+    print('------------------------------------------------------------------------')
+    print("Labels: ", y)
+    print('------------------------------------------------------------------------')
+    # define the number of kernels, channels and samples
+    kernels, chans, samples = 1, X.shape[1], X.shape[2]
+    # convert data to NHWC (trials, channels, samples, kernels) format. Set the number of kernels to 1.
+    X = X.reshape(X.shape[0], chans, samples, kernels)
+    # convert labels to one-hot encodings.
+    y = np_utils.to_categorical(y-1)
+    # return the data
+    return X, y, chans, samples
 
-# print the shape of the epochs and labels
-print("Shape of data: ", X.shape)
-print("Number of samples: ", X.shape[2])
-print("Shape of labels: ", y.shape)
+# function for training the model and performing k-fold cross validation
+def k_fold_cross_validation(X, y, chans, samples, num_folds, network):
 
-# change this so that it is automatically calculated!
-kernels = 1
-chans = X.shape[1] # 64
-samples = X.shape[2] # 751
+    # Define per-fold score containers
+    acc_per_fold = []
+    loss_per_fold = []
+    f1_per_fold = []
+    # Define the K-fold Cross Validator
+    kfold = KFold(n_splits=num_folds, shuffle=True)
+    # K-fold Cross Validation model evaluation
+    fold_no = 1
 
-# convert labels to one-hot encodings.
-y = np_utils.to_categorical(y-1)
+    for train, test in kfold.split(X, y):
+        if network == 'EEGNet':
+            # configure the EEGNet-8,2,16 model
+            model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples,
+                            dropoutRate = 0.5, kernLength = 125, F1 = 8, D = 2, F2 = 16,
+                            dropoutType = 'Dropout')
+        # compile the model and set the optimizers
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics = ['accuracy', f1_m, precision_m, recall_m])
+        # Generate a print
+        print('------------------------------------------------------------------------')
+        print(f'Training for fold {fold_no} ...')
+        # Fit data to model
+        history = model.fit(X[train], y[train],
+                            batch_size=16,
+                            epochs=100,
+                            verbose=2)
+        # Generate generalization metrics
+        scores = model.evaluate(X[test], y[test], verbose=0)
+        print(f'Score for fold {fold_no}: {model.metrics_names[0]}: {scores[0]}; {model.metrics_names[1]}: {scores[1]*100}%; {model.metrics_names[2]}: {scores[2]}; {model.metrics_names[3]}: {scores[3]}; {model.metrics_names[4]}: {scores[4]}')
+        acc_per_fold.append(scores[1] * 100)
+        loss_per_fold.append(scores[0])
+        f1_per_fold.append(scores[2])
+        # Increase fold number
+        fold_no = fold_no + 1
 
-# convert data to NHWC (trials, channels, samples, kernels) format. Data
-# contains 64 channels and 751 time-points. Set the number of kernels to 1.
-X = X.reshape(X.shape[0], chans, samples, kernels)
+    # == Provide average scores == (WRITE THIS TO A TXT FILE LATER)
+    print('------------------------------------------------------------------------')
+    print('Score per fold')
+    for i in range(0, len(acc_per_fold)):
+        print('------------------------------------------------------------------------')
+        print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}% - F1-Score: {f1_per_fold[i]}')
+    print('------------------------------------------------------------------------')
+    print('Average scores for all folds:')
+    print(f'> Accuracy: {np.mean(acc_per_fold)}% (+- {np.std(acc_per_fold)})')
+    print(f'> Loss: {np.mean(loss_per_fold)}')
+    print(f'> F1-Score: {np.mean(f1_per_fold)}')
+    print('------------------------------------------------------------------------')
 
-############################# K-FOLD CROSS VALIDATION ##################################
+##################### Run the code #####################
 
 num_folds = 5
+X, y, chans, samples = prepare_data(data_path, file_name, event_dict, class_labels=[5,6], tmin=0, tmax=3)
+k_fold_cross_validation(X, y, chans, samples, num_folds, 'EEGNet')
 
-# Define per-fold score containers
-acc_per_fold = []
-loss_per_fold = []
-
-# Define the K-fold Cross Validator
-kfold = KFold(n_splits=num_folds, shuffle=True)
-
-# K-fold Cross Validation model evaluation
-fold_no = 1
-for train, test in kfold.split(X, y):
-
-    # configure the EEGNet-8,2,16 model
-    model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples,
-                     dropoutRate = 0.5, kernLength = 125, F1 = 8, D = 2, F2 = 16,
-                     dropoutType = 'Dropout')
-    
-    # compile the model and set the optimizers
-    model.compile(
-        loss='binary_crossentropy',
-        optimizer='adam',
-        metrics = ['accuracy']
-    )
-
-    # Generate a print
-    print('------------------------------------------------------------------------')
-    print(f'Training for fold {fold_no} ...')
-
-    # Fit data to model
-    history = model.fit(X[train], y[train],
-                        batch_size=16,
-                        epochs=100,
-                        verbose=2)
-    
-    # Generate generalization metrics
-    scores = model.evaluate(X[test], y[test], verbose=0)
-    print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
-    acc_per_fold.append(scores[1] * 100)
-    loss_per_fold.append(scores[0])
-
-    # Increase fold number
-    fold_no = fold_no + 1
-
-# == Provide average scores ==
-print('------------------------------------------------------------------------')
-print('Score per fold')
-for i in range(0, len(acc_per_fold)):
-    print('------------------------------------------------------------------------')
-    print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
-print('------------------------------------------------------------------------')
-print('Average scores for all folds:')
-print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
-print(f'> Loss: {np.mean(loss_per_fold)}')
-print('------------------------------------------------------------------------')
-
-
-
-
-
-############################# EEGNet portion ##################################
-
-# # convert labels to one-hot encodings.
-# Y_train      = np_utils.to_categorical(Y_train-1)
-# Y_validate   = np_utils.to_categorical(Y_validate-1)
-# Y_test       = np_utils.to_categorical(Y_test-1)
-
-# # convert data to NHWC (trials, channels, samples, kernels) format. Data 
-# # contains 64 channels and 751 time-points. Set the number of kernels to 1.
-# X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
-# X_validate   = X_validate.reshape(X_validate.shape[0], chans, samples, kernels)
-# X_test       = X_test.reshape(X_test.shape[0], chans, samples, kernels)
-
-# print('X_train shape:', X_train.shape)
-# print(X_train.shape[0], 'train samples')
-# print(X_test.shape[0], 'test samples')
-
-# # configure the EEGNet-8,2,16 model with kernel length of 32 samples (other 
-# # model configurations may do better, but this is a good starting point)
-# model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples, 
-#                dropoutRate = 0.5, kernLength = 125, F1 = 8, D = 2, F2 = 16, 
-#                dropoutType = 'Dropout')
-
-# # compile the model and set the optimizers
-# model.compile(
-#     loss='binary_crossentropy', 
-#     optimizer='adam', 
-#     metrics = ['accuracy']
-# )
-
-# # count number of parameters in the model
-# numParams    = model.count_params()    
-
-# # set a valid path for your system to record model checkpoints
-# checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
-#                                save_best_only=True)
-
-# # fit the model
-# fittedModel = model.fit(X_train, Y_train, batch_size = 16, epochs = 300, 
-#                         verbose = 2, validation_data=(X_validate, Y_validate),
-#                         callbacks=[checkpointer])
-
-# # load optimal weights
-# model.load_weights('/tmp/checkpoint.h5')
-
-# # make prediction on test set
-# probs       = model.predict(X_test)
-# preds       = probs.argmax(axis = -1)  
-# acc         = np.mean(preds == Y_test.argmax(axis=-1))
-# print("Classification accuracy: %f " % (acc))
